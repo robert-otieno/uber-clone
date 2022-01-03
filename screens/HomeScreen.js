fabfc55ecc9c74fb482a65d59259e7100112b114
robert-otieno <robertotieno.or@gmail.com>
2022-01-03 21:36:44 +0300

    Implementing Google Autocomplete Library Pt1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
     return (
@@ -10,6 +12,21 @@ const HomeScreen = () => {
                 <Image 
                     style={{width: 100, height: 100, resizeMode: "contain"}}
                     source={{uri: "https://links.papareact.com/gzs"}}
+                />
+
+                <GooglePlacesAutocomplete
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    styles={{ container: { flex: 0, }, textInput: { fontSize:18, } }}
+                    debounce={400}
+                    placeholder='Where from?'
+                    // onPress={(data, details = null) => {
+                    //     // 'details' is provided when fetchDetails = true
+                    //     console.log(data, details);
+                    // }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
                 />
                 <NavOptions/>
             </View>
